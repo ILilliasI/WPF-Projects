27e62666a2877460b8a4a94e524a301befc64de8
Анна Сухова <agsukhova_1@edu.hse.ru>
2021-02-22 17:07:48 +0300

    init

---
+++ Stopwatch/Stopwatch/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Globalization;
+
+namespace Stopwatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool IsTimerEnabled { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RotateSecond(rotateSecond);
+            RotateMinute(rotateMinute, rotateSecond);
+
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            timer.Tick += ShowTime;
+        }
+
+        /// <summary>
+        /// Движение секундной стрелки секундомера.
+        /// </summary>
+        /// <param name="rotateSecond"></param>
+        static void RotateSecond(RotateTransform rotateSecond)
+        {
+            CompositionTarget.Rendering += (ss, ee) =>
+            {
+                rotateSecond.Angle = 6 * (stopwatch.ElapsedMilliseconds / 1000.0);
+            };
+        }
+
+        /// <summary>
+        /// Движение минутной стрелки секундомера.
+        /// </summary>
+        /// <param name="rotateMinute"></param>
+        private void RotateMinute(RotateTransform rotateMinute, RotateTransform rotateSecond)
+        {
+            CompositionTarget.Rendering += (ss, ee) =>
+            {
+                rotateMinute.Angle = stopwatch.ElapsedMilliseconds / 10000.0;
+            };
+        }
+
+        private static readonly DispatcherTimer timer = new DispatcherTimer();
+        private static readonly System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+        /// <summary>
+        /// Отображение отсчёта времени.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShowTime(object sender, EventArgs e)
+        {
+            Time.Text = stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fff", CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Включение/выключение секундомера.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsTimerEnabled)
+            {
+                IsTimerEnabled = true;
+                stopwatch.Start();
+                timer.Start();
+                StartStopButton.Content = "Стоп";
+                LapClearButton.Content = "Круг";
+            }
+            else
+            {
+                IsTimerEnabled = false;
+                stopwatch.Stop();
+                timer.Stop();
+                StartStopButton.Content = "Старт";
+                LapClearButton.Content = "Сброс";
+            }
+        }
+
+        /// <summary>
+        /// Сброс времени или фиксирование круга.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LapClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsTimerEnabled)
+            {
+                if (Time.Text == "00:00:00.000") return;
+                stopwatch.Restart();
+                stopwatch.Stop();
+                TimeIntervalsList.Items.Add(Time.Text);
+                TimeIntervalsList.Items.Add("---------------");
+                Time.Text = "00:00:00.000";
+            }
+            else
+            {
+                TimeIntervalsList.Items.Add(Time.Text);
+            }
+        }
+        
+        /// <summary>
+        /// Метод, выводящий информацию о макс., мин. и среднем временных интервалах.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TimeIntervalsList.Items.Count == 0)
+            {
+                MessageBox.Show($"Вы ещё не зафиксировали ни один временной интервал!");
+                return;
+            }
+            int cntTime = 0;
+            double maxTime = 0;
+            double minTime = -1;
+            double sumTime = 0;
+
+            foreach (string row in TimeIntervalsList.Items)
+            {
+                if (row != "---------------")
+                {
+                    cntTime++;
+                    string[] timeArr = row.Split(":");
+                    double currentLineTime = 0;
+                    // Степень для перевода времени в секунды.
+                    int pow60 = 2;
+
+                    foreach (string timeString in timeArr)
+                    {
+                        StringBuilder sb = new StringBuilder(timeString);
+                        char separator = Convert.ToChar(NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator);
+                        // Замена точки на системный разделитель для double, чтобы привести к double дробную часть секунд.
+                        sb.Replace('.', separator);
+                        double.TryParse(sb.ToString(), out double timeValue);
+
+                        currentLineTime += timeValue * Math.Pow(60, pow60--);
+                    }
+                    sumTime += currentLineTime;
+                    if ((minTime == -1) || (currentLineTime < minTime)) minTime = currentLineTime;
+                    if (currentLineTime > maxTime) maxTime = currentLineTime;
+                }
+            }
+            MessageBox.Show($"Максимальное время: {maxTime:F3}\nМинимальное время: {minTime:F3}\nСреднее время: {(sumTime / cntTime):F3}");
+        }
+    }
+}
---
+++ Task02/ButtonAndKeys/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonAndKeys
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int buttonPosition;
+
+        List<VerticalAlignment> possibleVerticalPositions = new List<VerticalAlignment>
+        { 0, 0, (VerticalAlignment)2, (VerticalAlignment)2};
+
+        List<HorizontalAlignment> possibleHorizontalPositions = new List<HorizontalAlignment>
+        { 0, (HorizontalAlignment)2, (HorizontalAlignment)2, 0 };
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    buttonPosition = (4 + --buttonPosition) % 4;
+                    break;
+                case Key.Right:
+                    buttonPosition = (buttonPosition + 1) % 4;
+                    break;
+            }
+
+            DontTouchMe.VerticalAlignment = possibleVerticalPositions[buttonPosition];
+            DontTouchMe.HorizontalAlignment = possibleHorizontalPositions[buttonPosition];
+
+        }
+    }
+}
